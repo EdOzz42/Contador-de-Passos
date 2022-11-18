bba18747f8e0520a00585e0b35400be175b73909
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-17 23:17:13 -0300

    I did it!!

--- javascript.js
+++ javascript.js
@@ -1,16 +1,39 @@
-var botao = document.querySelector('#buttonExec')
-var input1 = document.querySelector('#inpNume')
-botao.addEventListener('click', executar)
+let botao0 = document.querySelector('#buttonExec')
+botao0.addEventListener('click', executar)
+let botao1 = document.querySelector('#buttonDel')
+botao1.addEventListener('click', del)
 
-
-function executar() {
+/*function executar() {
+    var input = document.querySelector('#inpNume')
     let body = document.querySelector('body')
     let p = document.createElement('p')
     let n1 = input1 = String(input1.value)
     let txt = document.createTextNode(`Bla Bla 
     Bla ${n1}`)
     
-    
     p.appendChild(txt)
     body.appendChild(p)
+}
+*/
+
+function executar(){
+    let input = document.querySelector('#inpNume')
+    let contadorUser = Number(input.value)
+    let contadorAdd = 0
+    let section = document.createElement('section')
+    section.setAttribute('id', 'sctn')
+
+    while (contadorAdd < contadorUser) {
+        contadorAdd++
+        let main = document.querySelector('main')
+        let paragraph = document.createElement('p')
+        let content = document.createTextNode(`#${contadorAdd}`)
+        main.appendChild(section)
+        section.appendChild(paragraph)
+        paragraph.appendChild(content)
+    }
+}
+
+function del() {
+    document.querySelector('#sctn').remove()
 }
